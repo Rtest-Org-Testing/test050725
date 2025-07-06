cfb95cc75d3e6025358c9e0f5c2fe516481daf69
Rahul Joshi <rahul.joshi@nagarro.com>
2025-07-06 19:13:12 +0530

    move changes to main branch

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 #this is the code for the changes to be done in the main.tf to see the changes done in feature branch
 #this is a new branch with test 1
 #this is another change in the file Jira 004
+#thoda aur badal dete hai is file mai -jira 003- bahut abdiya
 
 
 
